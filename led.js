185aaa7187d4a469d044ed590b14b7f1637a5699
RTongue <r.tongue@gmail.com>
2018-01-31 23:33:01 -0500

    Hello World run for Led code

---
+++ led.js
@@ -0,0 +1,12 @@
+const Tessel = require('tessel-io');
+const five = require('johnny-five');
+
+const board = new five.Board({
+  io: new Tessel()
+});
+
+board.on('ready', () => {
+  const led = new five.Led('a5');
+  led.strobe(500);
+});
+
